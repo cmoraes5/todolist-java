6040896b9a6e180264a2878891e180c351b792c0
Caio Moraes V Santos <caio.moraes@radixeng.com.br>
2023-10-16 23:57:40 -0300

    aula 05

--- src/main/java/br/com/cmoraes5/todolist/task/TaskModel.java
+++ src/main/java/br/com/cmoraes5/todolist/task/TaskModel.java
@@ -14,7 +14,7 @@ import lombok.Data;
 @Data
 @Entity(name = "tb_tasks")
 public class TaskModel {
-  
+
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
@@ -25,9 +25,16 @@ public class TaskModel {
   private LocalDateTime startAt;
   private LocalDateTime endAt;
   private String priority;
-  
+
+  private UUID idUser;
+
   @CreationTimestamp
   private LocalDateTime createdAt;
 
-  private UUID idUser;
+  public void setTitle(String title) throws Exception {
+    if(title.length() > 50) {
+      throw new Exception("O campo title deve conter no máximo 50 caracteres");
+    }
+    this.title = title;
+  }
 }
